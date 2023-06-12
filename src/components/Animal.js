ed1d09df3cdd430336045cea478c9a863f47e6cd
Janice <janicenguyenn@gmail.com>
2023-06-12 15:05:07 -0700

    add Animal className

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,7 +1,7 @@
 import './Animal.css';
 
 const Animal = () => {
-return (<section>
+return (<section className='Animal'>
     <h3>Animal name: Willow</h3>
     <p>Species: Cat</p>
     <img src="http://placekitten.com/200/300"alt="Photograph of Willow"></img>
