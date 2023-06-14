fd0a7d160919d5b648d4d4725e6f2f05ead4d2e2
Janice <janicenguyenn@gmail.com>
2023-06-14 10:58:03 -0700

    add function to loop through objects

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -2,8 +2,8 @@ import './Animal.css';
 
 const Animal = (props) => {
 return (<section className='Animal'>
-    <h3>Animal name: Willow</h3>
-    <p>Species: Cat</p>
+    <h3>Animal name: {props.name}</h3>
+    <p>Species: {props.species}</p>
     <img src="http://placekitten.com/200/300"alt="Photograph of Willow"></img>
     </section>);
 
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -2,17 +2,38 @@ import './AnimalList.css'
 import Animal from './Animal';
 
 const AnimalList = () => {
-   return(
-    <section className="AnimalList">
-        <h2>Animal List</h2>
-        <ul className="Animal_list">
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
-        </ul>
-    </section>
-   )
-    
+    const firstAnimal = {
+        name: "Violet",
+        species: "pit mix"
+    };
+
+    const secondAnimal = {
+        name: "Norman",
+        species: "puppy"
+    }
+
+    const listOfAnimals = [
+        firstAnimal,
+        secondAnimal,
+    ]
+
+    const renderedAnimal = listOfAnimals.map(creature => (
+        <li><Animal
+            name={creature.name}
+            species={creature.species}
+        /> 
+        </li>
+    ));
+
+    return (
+        <section className="AnimalList">
+            <h2>Animal List</h2>
+            <ul className="Animal_list">
+                {renderedAnimal}
+            </ul>
+        </section>
+    )
+
 };
 
 export default AnimalList;
